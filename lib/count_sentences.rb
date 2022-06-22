06775c154b6d535c357783eb64a9bec006899323
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-06-21 22:49:24 -0400

    commit tree 9e2b0e516d8f9418b15a9c7481e4a7d6e77e8426

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/\.|\?|!/)
+    filtered_sentences = sentences.filter { |sentence| !sentence.empty? }
+    filtered_sentences.count
   end
+  
+  # binding.pry
+  
 end
+
